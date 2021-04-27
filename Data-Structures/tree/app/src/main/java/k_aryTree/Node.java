67717a403b2cec231ac8f2e8231dd4c5eabc676c
MHD22 <mhd.sad22@gmail.com>
2021-04-27 16:30:23 +0300

    challenge 18 is done FizzBuzzTree k-ary tree

---
+++ Data-Structures/tree/app/src/main/java/k_aryTree/Node.java
@@ -0,0 +1,38 @@
+package k_aryTree;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node <T>{
+    T value;
+    List<Node> children = new ArrayList<>();
+
+    public Node(T value) {
+        this.value = value;
+    }
+
+
+    public List<Node> getChildren() {
+        return children;
+    }
+
+    public void addChild(Node child) {
+        this.children.add(child);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result= new StringBuilder("{value= ");
+        result.append(value);
+        result.append("\nChildren: [");
+        for(Node ch : children) {
+            result.append("\n child: ");
+            result.append(ch);
+        }
+        result.append("]");
+
+        return result.toString();
+    }
+}
+
